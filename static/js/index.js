ca398c976bc79040a8fa8994edac96300360f4ea
gigatesseract <106117063@nitt.edu>
2019-03-29 23:08:46 +0530

    ui for requests

--- static/js/index.js
+++ static/js/index.js
@@ -25,6 +25,7 @@ function readURL(input) {
 }
 
 $(function() {
+  $("#video-player").hide();
   $("li").on("click", function() {
     console.log("clicked");
     $("ul.navbar-nav li").each(function() {
@@ -110,6 +111,7 @@ $(function() {
   $("#video-form").submit(function(e) {
     e.preventDefault();
     e.stopPropagation();
+    $("#video-place").text("Submitting...");
     var form_data = new FormData($("#video-form")[0]);
     console.log("submitted");
     $.ajax({
@@ -120,7 +122,7 @@ $(function() {
       data: form_data,
       cache: false,
       success: function(result) {
-        console.log(result);
+        $("#video-place").text("Hit the GET VIDEO button");
       },
       error: function(error) {
         console.log(error);
@@ -128,5 +130,90 @@ $(function() {
     });
 
     return false;
+  });
+  $("#get-video").on("click", function() {
+    console.log("data?: data");
+    jQuery.ajax({
+      url: "http://127.0.0.1:8765/recogniseFaces",
+      cache: false,
+      type: "GET",
+      processData: false,
+      xhr: function() {
+        var xhr = new XMLHttpRequest();
+        xhr.responseType = "blob";
+        return xhr;
+      },
+      success: function(data) {
+        if (data == null) console.log("fff");
+        console.log(data);
+        var link = document.createElement("a");
+        link.href = window.URL.createObjectURL(data);
+        link.download = "processed";
+
+        document.body.appendChild(link);
+
+        link.click();
+
+        document.body.removeChild(link);
+      },
+      error: function() {}
+    });
+  });
+  $("#feed-form").submit(function(e) {
+    console.log("feed.....");
+    e.preventDefault();
+    e.stopPropagation();
+    var form_data = new FormData($("#video-form")[0]);
+    form_data.append("name", $("#fname").val());
+    console.log("submitted");
+    $.ajax({
+      url: "http://127.0.0.1:8765/feedback",
+      type: "POST",
+      processData: false,
+      contentType: false,
+      data: form_data,
+      cache: false,
+      success: function(result) {
+        $("#feed-result").text("Successfully tuned");
+      },
+      error: function(error) {
+        console.log(error);
+      }
+    });
+
+    return false;
+  });
+  $("#get-names").on("click", function(e) {
+    console.log("name.....");
+    e.preventDefault();
+    e.stopPropagation();
+    // var form_data = new FormData($("#video-form")[0]);
+    // form_data.append("name", $("#fname").val());
+    console.log("submitted");
+    $.ajax({
+      url: "http://127.0.0.1:8765/names",
+      type: "GET",
+      processData: false,
+      contentType: false,
+      cache: false,
+      success: function(result) {
+        // console.log(result.names);
+        name_string = "<ol>";
+        result.names.forEach(ele => {
+          name_string += "<li>" + ele + "</li>";
+        });
+        name_string += "</ol>";
+        // console.log(name_string);
+        $("#name-result").html(name_string);
+      },
+      error: function(error) {
+        console.log(error);
+      }
+    });
+
+    return false;
+  });
+  $("#clear").on("click", function() {
+    $("#name-result").text("");
   });
 });
